0b38cbbf0760e90a9d7b016435651e0299633303
Naveen <naveens@tekion.com>
2021-05-09 09:42:21 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c34d0e0dc05ef41661487f0ee43d08d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '44c04104-58af-4f24-be56-d48a4bb21f26'
+};
